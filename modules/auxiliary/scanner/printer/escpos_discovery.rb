79adce02b481574670641271e76bd834e5583a6a
Mike <61128382+futileskills@users.noreply.github.com>
2025-08-18 05:34:04 -0500

    Update escpos_discovery.rb

--- modules/auxiliary/scanner/printer/escpos_discovery.rb
+++ modules/auxiliary/scanner/printer/escpos_discovery.rb
@@ -3,16 +3,15 @@ require 'msf/core'
 
 class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Scanner
-  include Msf::Auxiliary::Report
   include Msf::Exploit::Remote::Tcp
 
   def initialize(info = {})
     super(update_info(info,
-      'Name'        => 'ESC/POS Network Printer Discovery (Buffered Output)',
+      'Name'        => 'ESC/POS Network Printer Discovery (Thread-Safe, Clean Output)',
       'Description' => %q{
         Identifies network printers likely ESC/POS-compatible (Epson TM series, Star Micronics, BIXOLON)
         by checking TCP/9100 and optionally sending a safe ESC/POS status query.
-        Collects likely printer IPs and prints them all at the end.
+        Gives per-host status updates (if VERBOSE), collects likely printer IPs, and prints them all at the end.
       },
       'Author'      => ['FutileSkills'],
       'License'     => MSF_LICENSE
@@ -27,7 +26,9 @@ class MetasploitModule < Msf::Auxiliary
       ]
     )
 
-    @found_printers = []
+    # Thread-safe storage for found IPs
+    @@found_printers ||= []
+    @@found_printers_mutex ||= Mutex.new
   end
 
   DLE_EOT1 = "\x10\x04\x01".b
@@ -37,6 +38,7 @@ class MetasploitModule < Msf::Auxiliary
 
     begin
       connect(true)
+      vprint_status("#{ip}:#{rport} TCP open")
       if datastore['ACTIVE_CHECK']
         sock.put(DLE_EOT1)
         sock.flush
@@ -46,31 +48,25 @@ class MetasploitModule < Msf::Auxiliary
         likely = true  # TCP/9100 open is enough
       end
     rescue ::Rex::ConnectionError
+      vprint_status("#{ip}:#{rport} TCP closed")
       likely = false
     ensure
       disconnect rescue nil
     end
 
     if likely
-      @found_printers << ip
-
-      # Report to Metasploit database
-      store_service(host: ip, port: rport, proto: 'tcp', name: 'printer-raw-9100')
-      report_note(
-        host: ip,
-        type: 'printer.escpos.discovery',
-        data: { escpos_candidate: true },
-        update: true
-      )
+      @@found_printers_mutex.synchronize do
+        @@found_printers << ip
+      end
     end
   end
 
-  # This is called automatically by the scanner mixin after all hosts are scanned
+  # Called automatically after all hosts have been scanned
   def run_completed
-    return if @found_printers.empty?
+    return if @@found_printers.empty?
 
     print_good("\n=== Likely ESC/POS Printers Found ===")
-    @found_printers.uniq.each do |ip|
+    @@found_printers.uniq.sort.each do |ip|
       puts "[ESC/POS] #{ip}"
     end
     print_good("=== End of Scan ===\n")
